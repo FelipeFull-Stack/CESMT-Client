3c17f5672eb933b3b6d98dfd5f669a77ff236b15
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2023-02-10 11:36:41 -0300

    Routes & Route added in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </>
   );
 }
 
