835565d9d1663e37f717e818de7b91febe190232
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2023-02-11 11:45:15 -0300

    styling and construct Menubar.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { MenuBar } from "./pages/Menubar";
+import { Footer } from "./pages/Footer";
 
 function App() {
   return (
     <>
+      <MenuBar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </>
   );
 }
