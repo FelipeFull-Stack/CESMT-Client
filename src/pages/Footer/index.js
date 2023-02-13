790c4f912641aa7fe63197ce19f33109c950977c
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2023-02-13 08:51:30 -0300

    Style.css for Footer - done

--- src/pages/Footer/index.js
+++ src/pages/Footer/index.js
@@ -1,4 +1,8 @@
-import { FontAwesomeIcon } from "@fortawesome/fontawesome-svg-core";
+import instagram from "../../images/icones/instagram.png";
+import whatsapp from "../../images/icones/whatsapp.png";
+import phone from "../../images/icones/phone.png";
+import location from "../../images/icones/location.png";
+import email from "../../images/icones/email.png";
 
 function Footer() {
     return (
@@ -11,16 +15,140 @@ function Footer() {
                     background: "linear-gradient(to right, rgb(255, 45, 49), rgb(175, 45, 49))"
                 }}>
             </div>
-            <div>
-                <div>
 
+            <div
+                style={{ //div que abriga as outras divs
+                    border: "solid red 2px",
+                    height: "200px",
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    justifyContent: "flex-start",
+                    padding: "20px"
+                }}
+            >
+                <div
+                    style={{ //div que abriga os emails
+                        height: "80px",
+                        width: "350px",
+                        display: "flex",
+                        margin: "10px",
+                        flexDirection: "column",
+                        justifyContent: "baseline",
+                        alignItems: "baseline",
+                    }}
+                >
+                    <div
+                        style={{ //div da item 1
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center",
+                            gap: 5,
+                            marginBottom: "-40px"
+                        }}
+                    >
+                        <img style={{ width: "30px" }} src={email} alt="Email Icon" />
+                        <p style={{ fontSize: "24px", fontWeight: "700" }}>nilza@cesmt.com.br</p>
+                    </div>
+                    <div
+                        style={{ //div da item 2
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center",
+                            gap: 5
+                        }}
+                    >
+                        <img style={{ width: "30px" }} src={email} alt="Email Icon" />
+                        <p style={{ fontSize: "24px", fontWeight: "700", }}>cesmt@cesmt.com.br</p>
+                    </div>
                 </div>
-                <div>
-                <FontAwesomeIcon icon="fa-regular fa-phone" />
+
+                {/* <div> background: "linear-gradient(to right, rgb(255, 45, 49), rgb(175, 45, 49))" 
+                    <img style={{ width: "25px" }} src={instagram} alt="Instagram Icon" />
+                    <p></p>
+                </div> */}
+
+                <div
+                    style={{ //div que abriga whatsapp
+                        height: "80px",
+                        width: "350px",
+                        display: "flex",
+                        margin: "10px",
+                        justifyContent: "flex-start",
+                        alignItems: "center",
+                    }}
+                >
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center",
+                            gap: 5
+                        }}
+                    >
+                        <img style={{ width: "25px" }} src={whatsapp} alt="WhatsApp Icon" />
+                        <p style={{ fontSize: "24px", fontWeight: "700", }}>(73) 9 1234-5678</p>
+                    </div>
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
+
+                <div
+                    style={{ //div que abriga os telefones
+                        height: "80px",
+                        width: "350px",
+                        display: "flex",
+                        margin: "10px",
+                        flexDirection: "column",
+                        justifyContent: "baseline",
+                        alignItems: "baseline",
+                    }}
+                >
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center",
+                            gap: 5,
+                            marginBottom: "-40px"
+                        }}
+                    >
+                        <img style={{ width: "28px" }} src={phone} alt="Phone Icon" />
+                        <p style={{ fontSize: "24px", fontWeight: "700", }}>(73) 3526-1414</p>
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center",
+                            gap: 5
+                        }}
+                    >
+                        <img style={{ width: "28px" }} src={phone} alt="Phone Icon" />
+                        <p style={{ fontSize: "24px", fontWeight: "700", }}>(73) 3526-0718</p>
+                    </div>
+                </div>
+
+                <div
+                    style={{ //div que abriga localidade
+                        height: "80px",
+                        width: "470px",
+                        display: "flex",
+                        margin: "10px",
+                        justifyContent: "flex-start",
+                        alignItems: "center",
+                    }}
+                >
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center",
+                            gap: 5
+                        }}
+                    >
+                        <img style={{ width: "20px" }} src={location} alt="Location Icon" />
+                        <p style={{ fontSize: "24px", fontWeight: "700", }}>Rua Dr. José Alfredo Guimarães, Nº 26 Bairro São Luís, Jequié-BA - CEP: 45.203-330</p>
+                    </div>
+                </div>
                 <div></div>
             </div>
             <div
