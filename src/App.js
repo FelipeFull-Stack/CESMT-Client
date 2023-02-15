621aed966459b8a668244988b7964d280cac785e
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2023-02-15 14:10:58 -0300

    bug LogIn Page - solved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { SelectContextComponent } from "./context/SelectContext";
+import { AuthContextComponent } from "./context/authContext";
 import { Home } from "./pages/Home/index";
 import { Menubar } from "./pages/Menubar/index";
 import { Footer } from "./pages/Footer/index";
@@ -14,30 +15,32 @@ function App() {
   return (
     <>
       <SelectContextComponent>
-        <div
-          style={{
-            margin: 0,
-            padding: 0,
-            display: "block",
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%"
-          }}>
-          <Menubar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/contato" element={<Contato />} />
-            <Route path="/artigos" element={<Artigo />} />
-            <Route path="/servicos" element={<Servico />} />
-            <Route path="/clientes" element={<Cliente />} />
-            <Route path="/cadastro" element={<SignUp />} />
-            <Route path="/entrar" element={<LogIn />} />
-          </Routes>
-          <Footer />
-        </div>
+        <AuthContextComponent>
+          <div
+            style={{
+              margin: 0,
+              padding: 0,
+              display: "block",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%"
+            }}>
+            <Menubar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/contato" element={<Contato />} />
+              <Route path="/artigos" element={<Artigo />} />
+              <Route path="/servicos" element={<Servico />} />
+              <Route path="/clientes" element={<Cliente />} />
+              <Route path="/cadastro" element={<SignUp />} />
+              <Route path="/entrar" element={<LogIn />} />
+            </Routes>
+            <Footer />
+          </div>
+        </AuthContextComponent>
       </SelectContextComponent>
     </>
   );
