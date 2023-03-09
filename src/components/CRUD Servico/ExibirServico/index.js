554a375ebf1651d2dd649308fa68374a558bfa0d
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2023-03-09 18:42:46 -0300

    ServiçoPage iniciando transição de CSS Generico para MODULE

--- src/components/CRUD Servico/ExibirServico/index.js
+++ src/components/CRUD Servico/ExibirServico/index.js
@@ -1,152 +1,168 @@
 import { useState, useEffect } from "react";
 import { api } from "../../../api/api";
 import "./ExibirServico.css";
+import styles from "./ExibirServico.module.css";
 
 function ExibirServico() {
+	const [servico, setServico] = useState([]);
 
-    const [servico, setServico] = useState([]);
+	useEffect(() => {
+		async function fetchServico() {
+			try {
+				const response = await api.get("/servico");
+				setServico(response.data);
+			} catch (err) {
+				console.log(`Erro do Front-end em ExibirServico: ${err}`);
+			}
+		}
+		fetchServico();
+	}, []);
 
-    useEffect(() => {
-        async function fetchServico() {
-            try {
-                const response = await api.get("/servico");
-                setServico(response.data);
-            } catch (err) {
-                console.log(`Erro do Front-end em ExibirServico: ${err}`);
-            }
-        }
-        fetchServico();
-    }, [])
+	return (
+		<>
+			<div
+				className="div-geral-servico" /*className={styles.divPaiExibirServico}*/
+			>
+				{/* <div className={styles.divTituloExibirServico} ></div> */}
+				<h1>Nossos Serviços</h1>
+				<div className="div-servico-map" /*className={styles.divMapServicos}*/>
+					<div
+						className="div-titulo-servico" /*className={styles.divTituloServico}*/
+					>
+						<h1>Saúde e Segurança do Trabalho</h1>
+					</div>
+					{servico.map((currentElement) => {
+						if (currentElement.tipo === "SAUDE E SEGURANCA DO TRABALHO") {
+							return (
+								<>
+									<div
+										className="div-servico" /*className={styles.divServicoIndividualSST}*/
+									>
+										<h2>{currentElement.sigla}</h2>
+										<p>-</p>
+										<h3>{currentElement.nome}</h3>
+										{/* <p>{currentElement.description}</p> */}
+									</div>
+								</>
+							);
+						}
+						return null;
+					})}
+				</div>
 
-    return (
-        <>
-            <div className="div-geral-servico">
-                <h1 className="h1-servico">Nossos Serviços</h1>
-                <div className="div-servico-map">
-                    <div className="div-titulo-servico">
-                        <h1 className="titulo-servico"><b>Saúde e Segurança do Trabalho</b></h1>
-                    </div>
-                    {servico.map((currentElement) => {
-                        if (currentElement.tipo === "SAUDE E SEGURANCA DO TRABALHO") {
-                            return (
-                                <>
-                                    <div className="div-servico">
-                                        <div className="div-sigla-nome">
-                                            <h2 className="h2-servico">{currentElement.sigla}</h2>
-                                            <p className="hifem">-</p>
-                                            <h3 className="h3-servico">{currentElement.nome}</h3>
-                                        </div>
-                                        {/* <p>{currentElement.description}</p> */}
-                                    </div>
-                                </>
-                            );
-                        }
-                        return null;
-                    })}
-                </div>
+				<hr />
 
-                <hr />
+				<div className="div-servico-map" /*className={styles.divMapServicos}*/>
+					<div
+						className="div-titulo-servico" /*className={styles.divTituloServico}*/
+					>
+						<h1>Higiene Ocupacional</h1>
+					</div>
+					{servico.map((currentElement) => {
+						if (currentElement.tipo === "HIGIENE OCUPACIONAL") {
+							return (
+								<>
+									<div
+										className="div-servico" /*className={styles.divServicoIndividualHO}*/
+									>
+										<h2>{currentElement.sigla}</h2>
+										<p>-</p>
+										<h3>{currentElement.nome}</h3>
 
-                <div className="div-servico-map">
-                    <div className="div-titulo-servico">
-                        <h1 className="titulo-servico"><b>Higiene Ocupacional</b></h1>
-                    </div>
-                    {servico.map((currentElement) => {
-                        if (currentElement.tipo === "HIGIENE OCUPACIONAL") {
-                            return (
-                                <>
-                                    <div className="div-servico">
-                                        <div className="div-sigla-nome">
-                                            <h2 className="h2-servico">{currentElement.sigla}</h2>
-                                            <p className="hifem">-</p>
-                                            <h3 className="h3-servico">{currentElement.nome}</h3>
-                                        </div>
-                                        {/* <p>{currentElement.description}</p> */}
-                                    </div>
-                                </>
-                            );
-                        }
-                        return null;
-                    })}
-                </div>
+										{/* <p>{currentElement.description}</p> */}
+									</div>
+								</>
+							);
+						}
+						return null;
+					})}
+				</div>
 
-                <hr />
+				<hr />
 
-                <div className="div-servico-map">
-                    <div className="div-titulo-servico">
-                        <h1 className="titulo-servico"><b>Treinamentos</b></h1>
-                    </div>
-                    {servico.map((currentElement) => {
-                        if (currentElement.tipo === "TREINAMENTOS") {
-                            return (
-                                <>
-                                    <div className="div-servico">
-                                        <div className="div-sigla-nome">
-                                            <h2 className="h2-servico">{currentElement.sigla}</h2>
-                                            <p className="hifem">-</p>
-                                            <h3 className="h3-servico">{currentElement.nome}</h3>
-                                        </div>
-                                        {/* <p>{currentElement.description}</p> */}
-                                    </div>
-                                </>
-                            );
-                        }
-                        return null;
-                    })}
-                </div>
+				<div className="div-servico-map" /*className={styles.divMapServicos}*/>
+					<div
+						className="div-titulo-servico" /*className={styles.divTituloServico}*/
+					>
+						<h1>Treinamentos</h1>
+					</div>
+					{servico.map((currentElement) => {
+						if (currentElement.tipo === "TREINAMENTOS") {
+							return (
+								<>
+									<div
+										className="div-servico" /*className={styles.divServicoIndividualT}*/
+									>
+										<h2>{currentElement.sigla}</h2>
+										<p>-</p>
+										<h3>{currentElement.nome}</h3>
 
-                <hr />
+										{/* <p>{currentElement.description}</p> */}
+									</div>
+								</>
+							);
+						}
+						return null;
+					})}
+				</div>
 
-                <div className="div-servico-map">
-                    <div className="div-titulo-servico">
-                        <h1 className="titulo-servico"><b>Serviços</b></h1>
-                    </div>
-                    {servico.map((currentElement) => {
-                        if (currentElement.tipo === "SERVICOS") {
-                            return (
-                                <>
-                                    <div className="div-servico">
-                                        <div className="div-sigla-nome">
-                                            <h2 className="h2-servico">{currentElement.sigla}</h2>
-                                            {/* <p className="hifem">-</p> */}
-                                            <h3 className="h3-servico h3-extra">{currentElement.nome}</h3>
-                                        </div>
-                                        {/* <p>{currentElement.description}</p> */}
-                                    </div>
-                                </>
-                            );
-                        }
-                        return null;
-                    })}
-                </div>
+				<hr />
 
-                <hr />
+				<div className="div-servico-map" /*className={styles.divMapServicos}*/>
+					<div
+						className="div-titulo-servico" /*className={styles.divTituloServico}*/
+					>
+						<h1>Serviços</h1>
+					</div>
+					{servico.map((currentElement) => {
+						if (currentElement.tipo === "SERVICOS") {
+							return (
+								<>
+									<div
+										className="div-servico" /*className={styles.divServicoIndividualS}*/
+									>
+										{/* <h2>{currentElement.sigla}</h2> */}
+										{/* <p className="hifem">-</p> */}
+										<h3>{currentElement.nome}</h3>
 
-                <div className="div-servico-map">
-                    <div className="div-titulo-servico">
-                        <h1 className="titulo-servico"><b>Especialidades Médicas</b></h1>
-                    </div>
-                    {servico.map((currentElement) => {
-                        if (currentElement.tipo === "ESPECIALIDADES MEDICAS") {
-                            return (
-                                <>
-                                    <div className="div-servico">
-                                        <div className="div-sigla-nome">
-                                            <h2 className="h2-servico">{currentElement.sigla}</h2>
-                                            {/* <p className="hifem">-</p> */}
-                                            <h3 className="h3-servico h3-extra">{currentElement.nome}</h3>
-                                        </div>
-                                        {/* <p>{currentElement.description}</p> */}
-                                    </div>
-                                </>
-                            );
-                        }
-                        return null;
-                    })}
-                </div>
-            </div>
-        </>
-    );
-};
+										{/* <p>{currentElement.description}</p> */}
+									</div>
+								</>
+							);
+						}
+						return null;
+					})}
+				</div>
+
+				<hr />
+
+				<div className="div-servico-map" /*className={styles.divMapServicos}*/>
+					<div
+						className="div-titulo-servico" /*className={styles.divTituloServico}*/
+					>
+						<h1>Especialidades Médicas</h1>
+					</div>
+					{servico.map((currentElement) => {
+						if (currentElement.tipo === "ESPECIALIDADES MEDICAS") {
+							return (
+								<>
+									<div
+										className="div-servico" /*className={styles.divServicoIndividualEM}*/
+									>
+										{/* <h2>{currentElement.sigla}</h2> */}
+										{/* <p className="hifem">-</p> */}
+										<h3>{currentElement.nome}</h3>
+										{/* <p>{currentElement.description}</p> */}
+									</div>
+								</>
+							);
+						}
+						return null;
+					})}
+				</div>
+			</div>
+		</>
+	);
+}
 
 export { ExibirServico };
