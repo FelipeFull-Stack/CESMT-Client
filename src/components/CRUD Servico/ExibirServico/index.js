3b230752b3b981ed7ce61bf059031d3aa0c3935e
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2023-03-10 15:10:09 -0300

    adicionando requisito de Cidade para Serviços

--- src/components/CRUD Servico/ExibirServico/index.js
+++ src/components/CRUD Servico/ExibirServico/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { api } from "../../../api/api";
 import "./ExibirServico.css";
 import styles from "./ExibirServico.module.css";
@@ -18,32 +18,50 @@ function ExibirServico() {
 		fetchServico();
 	}, []);
 
+	const [exibirDescricao, setExibirDescricao] = useState(false);
+	const divRef = useRef(null);
+
+	function handleClick() {
+		setExibirDescricao(!exibirDescricao);
+	}
+
+	useEffect(() => {
+		function handleClickOutside(event) {
+			if (divRef.current && !divRef.current.contains(event.target)) {
+				setExibirDescricao(false);
+			}
+		}
+
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => document.removeEventListener("mousedown", handleClickOutside);
+	}, [divRef]);
+
 	return (
 		<>
-			<div
-				className="div-geral-servico" /*className={styles.divPaiExibirServico}*/
-			>
-				{/* <div className={styles.divTituloExibirServico} ></div> */}
-				<h1>Nossos Serviços</h1>
-				<div className="div-servico-map" /*className={styles.divMapServicos}*/>
-					<div
-						className="div-titulo-servico" /*className={styles.divTituloServico}*/
-					>
+			<div className={styles.divPaiExibirServico}>
+				<div className={styles.divTituloExibirServico}>
+					<h1>Serviços</h1>
+				</div>
+				<div className={styles.divMapServicos}>
+					<div className={styles.divTituloServico}>
 						<h1>Saúde e Segurança do Trabalho</h1>
 					</div>
 					{servico.map((currentElement) => {
 						if (currentElement.tipo === "SAUDE E SEGURANCA DO TRABALHO") {
 							return (
-								<>
-									<div
-										className="div-servico" /*className={styles.divServicoIndividualSST}*/
-									>
-										<h2>{currentElement.sigla}</h2>
-										<p>-</p>
-										<h3>{currentElement.nome}</h3>
-										{/* <p>{currentElement.description}</p> */}
-									</div>
-								</>
+								<div
+									className={`${styles.divServicoIndividualSST} ${
+										exibirDescricao
+											? styles.exibirDescricao
+											: styles.ocultarDescricao
+									}`}
+									ref={divRef}
+									onClick={handleClick}>
+									<h2>{currentElement.sigla}</h2>
+									<p>-</p>
+									<h3>{currentElement.nome}</h3>
+									<h4>{currentElement.description}</h4>
+								</div>
 							);
 						}
 						return null;
@@ -52,26 +70,26 @@ function ExibirServico() {
 
 				<hr />
 
-				<div className="div-servico-map" /*className={styles.divMapServicos}*/>
-					<div
-						className="div-titulo-servico" /*className={styles.divTituloServico}*/
-					>
+				<div className={styles.divMapServicos}>
+					<div className={styles.divTituloServico}>
 						<h1>Higiene Ocupacional</h1>
 					</div>
 					{servico.map((currentElement) => {
 						if (currentElement.tipo === "HIGIENE OCUPACIONAL") {
 							return (
-								<>
-									<div
-										className="div-servico" /*className={styles.divServicoIndividualHO}*/
-									>
-										<h2>{currentElement.sigla}</h2>
-										<p>-</p>
-										<h3>{currentElement.nome}</h3>
-
-										{/* <p>{currentElement.description}</p> */}
-									</div>
-								</>
+								<div
+									className={`${styles.divServicoIndividualHO} ${
+										exibirDescricao
+											? styles.exibirDescricao
+											: styles.ocultarDescricao
+									}`}
+									ref={divRef}
+									onClick={handleClick}>
+									<h2>{currentElement.sigla}</h2>
+									<p>-</p>
+									<h3>{currentElement.nome}</h3>
+									<h4>{currentElement.description}</h4>
+								</div>
 							);
 						}
 						return null;
@@ -80,26 +98,26 @@ function ExibirServico() {
 
 				<hr />
 
-				<div className="div-servico-map" /*className={styles.divMapServicos}*/>
-					<div
-						className="div-titulo-servico" /*className={styles.divTituloServico}*/
-					>
+				<div className={styles.divMapServicos}>
+					<div className={styles.divTituloServico}>
 						<h1>Treinamentos</h1>
 					</div>
 					{servico.map((currentElement) => {
 						if (currentElement.tipo === "TREINAMENTOS") {
 							return (
-								<>
-									<div
-										className="div-servico" /*className={styles.divServicoIndividualT}*/
-									>
-										<h2>{currentElement.sigla}</h2>
-										<p>-</p>
-										<h3>{currentElement.nome}</h3>
-
-										{/* <p>{currentElement.description}</p> */}
-									</div>
-								</>
+								<div
+									className={`${styles.divServicoIndividualT} ${
+										exibirDescricao
+											? styles.exibirDescricao
+											: styles.ocultarDescricao
+									}`}
+									ref={divRef}
+									onClick={handleClick}>
+									<h2>{currentElement.sigla}</h2>
+									<p>-</p>
+									<h3>{currentElement.nome}</h3>
+									<h4>{currentElement.description}</h4>
+								</div>
 							);
 						}
 						return null;
@@ -108,26 +126,26 @@ function ExibirServico() {
 
 				<hr />
 
-				<div className="div-servico-map" /*className={styles.divMapServicos}*/>
-					<div
-						className="div-titulo-servico" /*className={styles.divTituloServico}*/
-					>
+				<div className={styles.divMapServicos}>
+					<div className={styles.divTituloServico}>
 						<h1>Serviços</h1>
 					</div>
 					{servico.map((currentElement) => {
 						if (currentElement.tipo === "SERVICOS") {
 							return (
-								<>
-									<div
-										className="div-servico" /*className={styles.divServicoIndividualS}*/
-									>
-										{/* <h2>{currentElement.sigla}</h2> */}
-										{/* <p className="hifem">-</p> */}
-										<h3>{currentElement.nome}</h3>
-
-										{/* <p>{currentElement.description}</p> */}
-									</div>
-								</>
+								<div
+									className={`${styles.divServicoIndividualS} ${
+										exibirDescricao
+											? styles.exibirDescricao
+											: styles.ocultarDescricao
+									}`}
+									ref={divRef}
+									onClick={handleClick}>
+									<h2>{currentElement.sigla}</h2>
+									<p>-</p>
+									<h3>{currentElement.nome}</h3>
+									<h4>{currentElement.description}</h4>
+								</div>
 							);
 						}
 						return null;
@@ -136,25 +154,26 @@ function ExibirServico() {
 
 				<hr />
 
-				<div className="div-servico-map" /*className={styles.divMapServicos}*/>
-					<div
-						className="div-titulo-servico" /*className={styles.divTituloServico}*/
-					>
+				<div className={styles.divMapServicos}>
+					<div className={styles.divTituloServico}>
 						<h1>Especialidades Médicas</h1>
 					</div>
 					{servico.map((currentElement) => {
 						if (currentElement.tipo === "ESPECIALIDADES MEDICAS") {
 							return (
-								<>
-									<div
-										className="div-servico" /*className={styles.divServicoIndividualEM}*/
-									>
-										{/* <h2>{currentElement.sigla}</h2> */}
-										{/* <p className="hifem">-</p> */}
-										<h3>{currentElement.nome}</h3>
-										{/* <p>{currentElement.description}</p> */}
-									</div>
-								</>
+								<div
+									className={`${styles.divServicoIndividualEM} ${
+										exibirDescricao
+											? styles.exibirDescricao
+											: styles.ocultarDescricao
+									}`}
+									ref={divRef}
+									onClick={handleClick}>
+									<h2>{currentElement.sigla}</h2>
+									<p>-</p>
+									<h3>{currentElement.nome}</h3>
+									<h4>{currentElement.description}</h4>
+								</div>
 							);
 						}
 						return null;
